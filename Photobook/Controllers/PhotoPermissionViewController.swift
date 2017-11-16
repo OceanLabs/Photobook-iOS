139acb5fa3974c5fae29e43ea9eb35e4fa203b28
Julian Gruber <juliangruber7@gmail.com>
2017-11-16 09:54:59 +0000

    Intro screen on separate storyboard

--- Photobook/Controllers/PhotoPermissionViewController.swift
+++ Photobook/Controllers/PhotoPermissionViewController.swift
@@ -10,11 +10,53 @@ import UIKit
 import Photos
 
 class PhotoPermissionViewController: UIViewController {
-
-    @IBOutlet weak var ctaButton: UIButton!
+    
+    @IBOutlet weak var ctaContainerView: UIView!
+    @IBOutlet weak var bgImageView: UIImageView!
+    
+    @IBOutlet weak var ctaVisibleConstraint: NSLayoutConstraint!
+    @IBOutlet weak var ctaInvisibleConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        self.ctaVisibleConstraint.priority = .defaultLow
+        self.ctaInvisibleConstraint.priority = .init(751)
+        
+        self.view.layoutIfNeeded()
+        
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(appWillTerminate), name: Notification.Name.UIApplicationWillTerminate, object: nil)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if PHPhotoLibrary.authorizationStatus() == .authorized {
+            self.dismiss(animated: false, completion: {
+                
+            })
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let animationDuration:Double = 0.7
+        
+        self.view.layoutIfNeeded()
+        
+        UIView.animateKeyframes(withDuration: animationDuration, delay: 0, options: .calculationModePaced, animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 1, animations: {
+                self.ctaVisibleConstraint.priority = .init(751)
+                self.ctaInvisibleConstraint.priority = .defaultLow
+                
+                self.view.layoutIfNeeded()
+            })
+        }) { (completed) in
+            
+        }
 
     }
     
@@ -45,16 +87,38 @@ class PhotoPermissionViewController: UIViewController {
         }
     }
     
+    @objc func appWillTerminate() {
+        self.ctaVisibleConstraint.priority = .defaultLow
+        self.ctaInvisibleConstraint.priority = .init(751)
+        
+        self.view.layoutIfNeeded()
+    }
+    
     func showPermissionDeniedDialog() {
-        let alert = UIAlertController(title: "Alert", message: "Photo access is restricted, but needed to create beautiful photo books.\nYou can turn it on in the system settings", preferredStyle: UIAlertControllerStyle.alert)
-        alert.addAction(UIAlertAction(title: "Open Settings", style: UIAlertActionStyle.default, handler: { (action) in
+        
+        let alertText = NSLocalizedString("Controllers/PhotoPermissionController/PermissionDeniedDialogText",
+                                                       value: "Photo access is restricted, but it's needed to create beautiful photo books.\nYou can turn it on in the system settings",
+                                                       comment: "Alert dialog when photo library access has been disabled")
+        let alertTitle = NSLocalizedString("Controllers/PhotoPermissionController/PermissionDeniedDialogTitle",
+                                                       value: "Hm...",
+                                                       comment: "Alert dialog when photo library access has been disabled")
+        let alertOpenSettings = NSLocalizedString("Controllers/PhotoPermissionController/PermissionDeniedDialogOpenSettings",
+                                                 value: "Open Settings",
+                                                 comment: "Alert dialog button when photo library access has been disabled")
+        let alertOK = NSLocalizedString("Controllers/PhotoPermissionController/PermissionDeniedDialogOK",
+                                                  value: "Open Settings",
+                                                  comment: "Alert dialog button when photo library access has been disabled")
+        
+        
+        let alert = UIAlertController(title: alertTitle, message: alertText, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: alertOpenSettings, style: UIAlertActionStyle.default, handler: { (action) in
             if let appSettings = URL(string: UIApplicationOpenSettingsURLString) {
                 UIApplication.shared.open(appSettings, options: [:], completionHandler: { (success) in
                     
                 })
             }
         }))
-        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: { (action) in
+        alert.addAction(UIAlertAction(title: alertOK, style: UIAlertActionStyle.cancel, handler: { (action) in
             
         }))
         self.present(alert, animated: true, completion: nil)
