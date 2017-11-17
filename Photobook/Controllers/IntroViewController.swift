c30955995db28f557b989ef439c3753decac78ba
Julian Gruber <juliangruber7@gmail.com>
2017-11-17 12:05:32 +0000

    Cleanup
    Removed empty completion block

--- Photobook/Controllers/IntroViewController.swift
+++ Photobook/Controllers/IntroViewController.swift
@@ -63,9 +63,7 @@ class IntroViewController: UIViewController {
                 
                 self.view.layoutIfNeeded()
             })
-        }) { (completed) in
-            
-        }
+        })
 
     }
     
