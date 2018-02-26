c54b1e4e27eb390162f3d2a17f65475c0dfba85a
Jaime Landazuri <jaime@kite.ly>
2018-02-26 12:36:28 +0000

    PHO-64: Removes test code

--- Photobook/Model/Photobooks/ProductManager.swift
+++ Photobook/Model/Photobooks/ProductManager.swift
@@ -85,7 +85,6 @@ class ProductManager {
     var pageColor: ProductColor = .white
     var productLayouts = [ProductLayout]()
     var minimumRequiredAssets: Int {
-        return 4
         let defaultMinimum = 20
         
         return product?.minimumRequiredAssets ?? products?.first?.minimumRequiredAssets ?? defaultMinimum
