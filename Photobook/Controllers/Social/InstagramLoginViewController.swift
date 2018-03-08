7951dfa7f624cd8097d7f9ca59b7d6d300ad84a2
Kostas Karayannis <kkarayannis@gmail.com>
2018-03-08 09:59:35 +0100

    PHO-20: Fixed Instagram login webview position

--- Photobook/Controllers/Social/InstagramLoginViewController.swift
+++ Photobook/Controllers/Social/InstagramLoginViewController.swift
@@ -30,8 +30,21 @@ class InstagramLoginViewController: UIViewController {
         super.viewDidLoad()
         
         webView.navigationDelegate = self
-        webView.frame = view.bounds
         view.insertSubview(webView, at: 0)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        if #available(iOS 11.0, *) {
+            let guide = self.view.safeAreaLayoutGuide
+            webView.trailingAnchor.constraint(equalTo: guide.trailingAnchor).isActive = true
+            webView.leadingAnchor.constraint(equalTo: guide.leadingAnchor).isActive = true
+            webView.bottomAnchor.constraint(equalTo: guide.bottomAnchor).isActive = true
+            webView.topAnchor.constraint(equalTo: guide.topAnchor).isActive = true
+        } else {
+            NSLayoutConstraint(item: webView, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1.0, constant: 0).isActive = true
+            NSLayoutConstraint(item: webView, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1.0, constant: 0).isActive = true
+            NSLayoutConstraint(item: webView, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1.0, constant: 0).isActive = true
+            NSLayoutConstraint(item: webView, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1.0, constant: 0).isActive = true
+        }
+        
         
         startAuthenticatingUser()
     }
