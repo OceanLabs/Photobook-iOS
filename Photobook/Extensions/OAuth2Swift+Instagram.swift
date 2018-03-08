4171320024878b38a1857e75aeef2b49dcfd1823
Kostas Karayannis <kkarayannis@gmail.com>
2018-03-08 10:35:44 +0100

    PHO-20: Updated Instagram credentials

--- Photobook/Extensions/OAuth2Swift+Instagram.swift
+++ Photobook/Extensions/OAuth2Swift+Instagram.swift
@@ -11,11 +11,11 @@ import OAuthSwift
 extension OAuth2Swift {
     
     struct Constants {
-        static let clientId = "94de981b3c744775a41c24ceac0c7609"
-        static let secret = "df7bd131253043e693c266a3b4105149"
+        static let clientId = "9e18da2dd4fa41e495cf769a18259595"
+        static let secret = "7cf3519040074be78bf47d4270556937"
         static let instagramAuthUrlString = "https://api.instagram.com/oauth/authorize"
         static let keychainInstagramTokenKey = "InstagramTokenKey"
-        static let redirectUri = "https://kite.ly/instagram-callback"
+        static let redirectUri = "https://kite.ly/instagram-callback/photobook-ios"
         static let scope = "basic"
     }
     
