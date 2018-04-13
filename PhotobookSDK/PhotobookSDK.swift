78d7ad9bd4edde86890a3ceddeaec90dcb53ffdf
Kostas Karayannis <kkarayannis@gmail.com>
2018-04-13 12:36:38 +0200

    PHO-199: Fixed framework build

--- PhotobookSDK/PhotobookSDK.swift
+++ PhotobookSDK/PhotobookSDK.swift
@@ -93,6 +93,7 @@ import UIKit
     ///
     /// - Parameter completionHandler: Completion handler to be forwarded from 'handleEventsForBackgroundURLSession' in the application's app delegate
     @objc public func restorePhotobook(_ completionHandler: @escaping (() -> Void)) {
-        ProductManager.shared.currentProduct!.loadUserPhotobook(completionHandler)
+        ProductManager.shared.loadUserPhotobook()
+        completionHandler()
     }
 }
