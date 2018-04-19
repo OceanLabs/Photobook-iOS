e219eac23ac858a4c5b4f75dcb058000d7347f15
Jaime Landazuri <jaime@kite.ly>
2018-04-19 13:42:51 +0100

    PHO-207: Adds tests for recognizer transformation util

--- PhotobookTests/Model/Photobooks/LayoutUtilsTests.swift
+++ PhotobookTests/Model/Photobooks/LayoutUtilsTests.swift
@@ -9,11 +9,20 @@
 import XCTest
 @testable import Photobook
 
+class TestPanGestureRecognizer: UIPanGestureRecognizer {
+    var x, y: CGFloat!
+    
+    override func translation(in view: UIView?) -> CGPoint {
+        return CGPoint(x: x, y: y)
+    }
+}
+
 class LayoutUtilsTests: XCTestCase {
     
-    let containerSize = CGSize(width: 1.0, height: 1.0)
-    
     private let identity = CGAffineTransform.identity
+    private let containerSize = CGSize(width: 1.0, height: 1.0)
+    private let parentView = UIView()
+    private let initialTransform = CGAffineTransform.identity.translatedBy(x: 5.0, y: 6.0).scaledBy(x: 2.0, y: 2.0).rotated(by: .pi / 6.0)
     
     func testScaleToFill() {
         let images = [
@@ -63,5 +72,46 @@ class LayoutUtilsTests: XCTestCase {
         
         let newTransform = LayoutUtils.centerTransform(transform, inParentView: parentView, fromPoint: currentPoint)
         XCTAssertEqual(newTransform, expectedTransform)
+    }
+    
+    func testAdjustTransformWithRecognizer_rotatesTransform() {
+        let rotationGestureRecognizer = UIRotationGestureRecognizer()
+        rotationGestureRecognizer.rotation = .pi / 6.0 // 30 degrees
+        
+        let newTransform = LayoutUtils.adjustTransform(initialTransform, withRecognizer: rotationGestureRecognizer, inParentView: parentView)
+        
+        // Original rotation .pi / 6 + additional .pi / 6 = result .pi / 3
+        XCTAssertEqual(newTransform.angle, .pi / 3.0, accuracy: 0.01)
+    }
+    
+    func testAdjustTransformWithRecognizer_scalesTransform() {
+        let pinchGestureRecognizer = UIPinchGestureRecognizer()
+        pinchGestureRecognizer.scale = 2.0
+        
+        let newTransform = LayoutUtils.adjustTransform(initialTransform, withRecognizer: pinchGestureRecognizer, inParentView: parentView)
+        
+        // Original scale 2 x new scale 2 = result 4
+        XCTAssertEqual(newTransform.scale, 4.0, accuracy: 0.01)
+    }
+    
+    func testAdjustTransformWithRecognizer_maxScale() {
+        let pinchGestureRecognizer = UIPinchGestureRecognizer()
+        pinchGestureRecognizer.scale = 10.0
+
+        let newTransform = LayoutUtils.adjustTransform(initialTransform, withRecognizer: pinchGestureRecognizer, inParentView: parentView, maxScale: 3.0)
+        
+        // Original scale 2 x new scale 10.0 = result max Scale 3.0
+        XCTAssertEqual(newTransform.scale, 3.0, accuracy: 0.01)
+    }
+    
+    func testAdjustTransformWithRecognizer_pansTransform() {
+        let panGestureRecognizer = TestPanGestureRecognizer()
+        panGestureRecognizer.x = 4.0
+        panGestureRecognizer.y = 6.0
+        
+        let newTransform = LayoutUtils.adjustTransform(initialTransform, withRecognizer: panGestureRecognizer, inParentView: parentView)
+        
+        // Original translation (5.0, 6.0) + new translation (4.0, 6.0) = result (9.0, 12.0)
+        XCTAssertTrue(newTransform.tx == 9.0 && newTransform.ty == 12.0)
     }
 }
