488ea2c6ba181a72bbb6b3aaf25fe4ca6477802d
Jaime Landazuri <jaime@kite.ly>
2018-04-23 12:20:56 +0100

    PHO-207: Adds tests for ProductLayoutAsset

--- PhotobookTests/Model/Photobooks/ProductLayoutAssetTests.swift
+++ PhotobookTests/Model/Photobooks/ProductLayoutAssetTests.swift
@@ -49,10 +49,82 @@ class ProductLayoutAssetTests: XCTestCase {
         XCTAssertNotNil(asset, "The asset should be a PhotosAsset")
         
         let transform = unarchivedProductLayoutAsset.transform
-        XCTAssertTrue(transform == originalTransform, "The decoded transform must match the original transform")
+        XCTAssertTrue(transform ==~ originalTransform, "The decoded transform must match the original transform")
         
         XCTAssertTrue(unarchivedProductLayoutAsset.containerSize.width == originalContainerSize.width
             && unarchivedProductLayoutAsset.containerSize.height == originalContainerSize.height, "The decoded container size must match the original size")
     }
     
+    // MARK: ContainerSize didSet
+    func testContainerSize_fitsAssetIfFirstTime() {
+        let productLayoutAsset = ProductLayoutAsset()
+        productLayoutAsset.asset = photosAsset
+        productLayoutAsset.containerSize = CGSize(width: 120.0, height: 100.0)
+        let expectedTransform = CGAffineTransform(a: 0.05, b: 0.0, c: 0.0, d: 0.05, tx: 0.0, ty: 0.0)
+        XCTAssertTrue(productLayoutAsset.transform ==~ expectedTransform)
+    }
+    
+    func testContainerSize_doesNotFitAssetIfSecondTime() {
+        let productLayoutAsset = ProductLayoutAsset()
+        productLayoutAsset.asset = photosAsset
+        productLayoutAsset.containerSize = CGSize(width: 120.0, height: 100.0)
+        productLayoutAsset.transform = CGAffineTransform.identity
+        productLayoutAsset.containerSize = CGSize(width: 120.0, height: 100.0)
+        
+        let expectedTransform = CGAffineTransform(a: 0.05, b: 0.0, c: 0.0, d: 0.05, tx: 0.0, ty: 0.0)
+        XCTAssertFalse(productLayoutAsset.transform ==~ expectedTransform)
+    }
+
+    func testContainerSize_fitsAssetIfShouldFitAssetIsTrue() {
+        let productLayoutAsset = ProductLayoutAsset()
+        productLayoutAsset.asset = photosAsset
+        productLayoutAsset.containerSize = CGSize(width: 120.0, height: 100.0)
+        productLayoutAsset.transform = CGAffineTransform.identity
+        productLayoutAsset.shouldFitAsset = true
+        productLayoutAsset.containerSize = CGSize(width: 120.0, height: 100.0)
+
+        let expectedTransform = CGAffineTransform(a: 0.05, b: 0.0, c: 0.0, d: 0.05, tx: 0.0, ty: 0.0)
+        XCTAssertTrue(productLayoutAsset.transform ==~ expectedTransform)
+    }
+    
+    func testContainerSize_adjustsTransformWithAfterSizeChangeToSmallerContainer() {
+        let productLayoutAsset = ProductLayoutAsset()
+        productLayoutAsset.asset = photosAsset
+        productLayoutAsset.containerSize = CGSize(width: 120.0, height: 100.0)
+        productLayoutAsset.containerSize = CGSize(width: 110.0, height: 80.0)
+        
+        let expectedTransform = CGAffineTransform(a: 0.046, b: 0.0, c: 0.0, d: 0.046, tx: 0.0, ty: 0.0)
+        XCTAssertTrue(productLayoutAsset.transform ==~ expectedTransform)
+    }
+    
+    func testContainerSize_adjustsTransformWithAfterSizeChangeToBiggerContainer() {
+        let productLayoutAsset = ProductLayoutAsset()
+        productLayoutAsset.asset = photosAsset
+        productLayoutAsset.containerSize = CGSize(width: 120.0, height: 100.0)
+        productLayoutAsset.containerSize = CGSize(width: 230.0, height: 300.0)
+        
+        let expectedTransform = CGAffineTransform(a: 0.15, b: 0.0, c: 0.0, d: 0.15, tx: 0.0, ty: 0.0)
+        XCTAssertTrue(productLayoutAsset.transform ==~ expectedTransform)
+    }
+
+    // MARK: - Shallow copy
+    func testShallowCopy() {
+        let image = UIImage()
+        
+        let productLayoutAsset = ProductLayoutAsset()
+        productLayoutAsset.asset = photosAsset
+        productLayoutAsset.containerSize = CGSize(width: 120.0, height: 100.0)
+        productLayoutAsset.currentIdentifier = "1"
+        productLayoutAsset.currentImage = image
+        
+        let productLayoutAssetCopy = productLayoutAsset.shallowCopy()
+        XCTAssertTrue(productLayoutAssetCopy.asset != nil &&
+            productLayoutAssetCopy.currentImage != nil &&
+            productLayoutAssetCopy.currentIdentifier != nil &&
+            productLayoutAssetCopy.asset! == productLayoutAsset.asset! &&
+            productLayoutAssetCopy.containerSize == productLayoutAsset.containerSize &&
+            productLayoutAssetCopy.transform == productLayoutAsset.transform &&
+            productLayoutAssetCopy.currentIdentifier! == productLayoutAsset.currentIdentifier! &&
+            productLayoutAssetCopy.currentImage! == productLayoutAsset.currentImage!)
+    }
 }
