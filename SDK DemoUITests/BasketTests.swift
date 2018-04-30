078e14f25290d9011047734ceb1690f1e78fdccf
Kostas Karayannis <kkarayannis@gmail.com>
2018-04-30 10:31:00 +0200

    PHO-223: Added invalid promo code test

--- SDK DemoUITests/BasketTests.swift
+++ SDK DemoUITests/BasketTests.swift
@@ -18,4 +18,24 @@ extension SDK_DemoUITests {
         automation.app.buttons["Done"].tap()
     }
     
+    func testInvalidPromoCode() {
+        automation.goToBasket()
+        
+        let promoCodeTextField = automation.app.textFields["promoCodeTextField"]
+        promoCodeTextField.tap()
+        
+        let invalidPromoCode = "This is definitely a valid promo code"
+        promoCodeTextField.typeText(invalidPromoCode + "\n")
+        
+        let predicate = NSPredicate(format: "value != \"\(invalidPromoCode)\"")
+        expectation(for: predicate, evaluatedWith: promoCodeTextField, handler: nil)
+        waitForExpectations(timeout: 60, handler: nil)
+        
+        let newTextFieldValue = promoCodeTextField.value as? String
+        XCTAssertNotNil(newTextFieldValue)
+        XCTAssert(newTextFieldValue!.contains("Invalid code"), "Invalid code messsage not shown")
+    }
+    
+    
+    
 }
