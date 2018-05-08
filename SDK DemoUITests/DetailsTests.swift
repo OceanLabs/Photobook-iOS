d04f9524921bccf4dc44ac3d7f7e3e6f122460a4
Kostas Karayannis <kkarayannis@gmail.com>
2018-05-08 15:17:51 +0200

    PHO-229: Added tests for email and phone validation

--- SDK DemoUITests/DetailsTests.swift
+++ SDK DemoUITests/DetailsTests.swift
@@ -14,10 +14,10 @@ class DetailsTests: PhotobookUITest {
         automation.goToBasket()
         automation.goToDeliveryDetailsFromBasket()
         
-        let nameTextField = automation.app.textFields["nameTextField"]
-        let lastNameTextField = automation.app.textFields["lastNameTextField"]
-        let emailTextField = automation.app.textFields["emailTextField"]
-        let phoneTextField = automation.app.textFields["phoneTextField"]
+        let nameTextField = automation.app.cells["nameCell"].textFields["userInputTextField"]
+        let lastNameTextField = automation.app.cells["lastNameCell"].textFields["userInputTextField"]
+        let emailTextField = automation.app.cells["emailCell"].textFields["userInputTextField"]
+        let phoneTextField = automation.app.cells["phoneCell"].textFields["userInputTextField"]
         
         XCTAssertNotNil(nameTextField.value)
         XCTAssertEqual(nameTextField.value as! String, "Required")
@@ -32,7 +32,7 @@ class DetailsTests: PhotobookUITest {
         XCTAssertEqual(phoneTextField.value as! String, "Required")
         
         let addressErrorMessageLabel = automation.app.staticTexts["addressErrorMessageLabel"]
-        XCTAssertEqual(addressErrorMessageLabel.value as! String, "")
+        XCTAssertEqual(addressErrorMessageLabel.label, "")
         
         automation.app.navigationBars["Delivery Details"].buttons["Save"].tap()
         XCTAssertTrue(nameTextField.isHittable, "Should not have navigated away since the required information is not entered")
@@ -66,6 +66,36 @@ class DetailsTests: PhotobookUITest {
         automation.app.navigationBars["Delivery Details"].buttons["Save"].tap()
         
         XCTAssertFalse(nameTextField.exists, "We should have all the required information at this point so we should have navigated away")
+    }
+    
+    func testInvalidInput() {
+        automation.goToBasket()
+        automation.goToDeliveryDetailsFromBasket()
+        
+        let emailTextField = automation.app.cells["emailCell"].textFields["userInputTextField"]
+        let phoneTextField = automation.app.cells["phoneCell"].textFields["userInputTextField"]
+        let emailMessageLabel = automation.app.cells["emailCell"].staticTexts["userInputMessageLabel"]
+        let phoneMessageLabel = automation.app.cells["phoneCell"].staticTexts["userInputMessageLabel"]
+        
+        // Email
+        XCTAssertEqual(emailMessageLabel.label, "", "We should not be showing an error message at this time")
+        emailTextField.tap()
+        emailTextField.typeText("i.am.a.clown.email.com\n")
+        XCTAssertEqual(emailMessageLabel.label, "Email is invalid", "We should be showing an error message at this time")
+        
+        emailTextField.tap()
+        emailTextField.typeText("this.is.a.valid.email@tada.com\n")
+        XCTAssertEqual(emailMessageLabel.label, "", "The email is valid. We should not be showing an error message")
+        
+        // Phone
+        XCTAssertEqual(phoneMessageLabel.label, "Required by the postal service in case there are any issues with the delivery", "We should not be showing an error message at this time")
+        phoneTextField.tap()
+        phoneTextField.typeText("1234\n")
+        XCTAssertEqual(phoneMessageLabel.label, "Phone is invalid", "We should be showing an error message at this time")
+        
+        phoneTextField.tap()
+        phoneTextField.typeText("1234567890\n")
+        XCTAssertEqual(phoneMessageLabel.label, "Required by the postal service in case there are any issues with the delivery", "The phone is valid. We should not be showing an error message")
     }
 
 }
