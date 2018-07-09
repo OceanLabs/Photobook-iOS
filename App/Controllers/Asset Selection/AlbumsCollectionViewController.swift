e1409094b83538bafc3f198e5313c5081230fe18
Jaime Landazuri <jaime@kite.ly>
2018-07-09 13:43:11 +0100

    PHO-284: Adds missing Photobook VC closure when navigating from albums

--- App/Controllers/Asset Selection/AlbumsCollectionViewController.swift
+++ App/Controllers/Asset Selection/AlbumsCollectionViewController.swift
@@ -267,7 +267,13 @@ extension AlbumsCollectionViewController: AssetCollectorViewControllerDelegate {
             let photobookViewController = photobookMainStoryboard.instantiateViewController(withIdentifier: "PhotobookViewController") as! PhotobookViewController
             photobookViewController.assets = selectedAssetsManager.selectedAssets
             photobookViewController.photobookDelegate = self
-            
+            photobookViewController.completionClosure = { (photobookProduct) in
+                OrderManager.shared.basketOrder.products = [photobookProduct]
+                
+                let checkoutViewController = photobookMainStoryboard.instantiateViewController(withIdentifier: "CheckoutViewController") as! CheckoutViewController
+                photobookViewController.navigationController?.pushViewController(checkoutViewController, animated: true)
+            }
+
             navigationController?.pushViewController(photobookViewController, animated: true)
         }
     }
