156365a2d2426696bb9acbf7de3f7e31e31a47c4
Deon Botha <deon@dbotha.com>
2018-08-23 15:08:48 +0100

    Release keys for PicCollage

--- Pico Book App/Configuration.swift
+++ Pico Book App/Configuration.swift
@@ -10,7 +10,7 @@ import Foundation
 
 struct Configuration {
     static let shouldIntroBackgroundImageScroll = false
-    static let kiteApiClientKey = "0453d74be957c1eb510fc2d580007294cdc31a79" //Live: 97252fff0fb3061a53b421156e9049917f81c32d
+    static let kiteApiClientKey = "97252fff0fb3061a53b421156e9049917f81c32d" //Test: 0453d74be957c1eb510fc2d580007294cdc31a79
     static let applePayPayToString = "Kite.ly (via PicoBook)"
     static let applePayMerchantId = "merchant.com.cardinalblue.Picobook"
 }
