1ced69c2f584963ec513b2f9722f88c040266269
Kostas Karayannis <kkarayannis@gmail.com>
2018-10-08 16:28:04 +0200

    Remove PicoBook assets

--- Pico Book App/Configuration.swift
+++ Pico Book App/Configuration.swift
@@ -10,8 +10,8 @@ import Foundation
 
 struct Configuration {
     static let shouldIntroBackgroundImageScroll = false
-    static let kiteApiClientLiveKey = "97252fff0fb3061a53b421156e9049917f81c32d"
-    static let kiteApiClientTestKey = "0453d74be957c1eb510fc2d580007294cdc31a79"
+    static let kiteApiClientLiveKey = ""
+    static let kiteApiClientTestKey = ""
     static let applePayPayToString = "Kite.ly (via PicoBook)"
-    static let applePayMerchantId = "merchant.com.cardinalblue.Picobook"
+    static let applePayMerchantId = ""
 }
