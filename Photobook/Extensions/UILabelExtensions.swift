88931a361025cd375d4e54a9a6a0e977902a0e6f
Jaime Landazuri <jaime@kite.ly>
2018-11-08 10:35:44 +0000

    Adds label extensions to photo book target

---
+++ Photobook/Extensions/UILabelExtensions.swift
@@ -0,0 +1,52 @@
+//
+//  Modified MIT License
+//
+//  Copyright (c) 2010-2018 Kite Tech Ltd. https://www.kite.ly
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The software MAY ONLY be used with the Kite Tech Ltd platform and MAY NOT be modified
+//  to be used with any competitor platforms. This means the software MAY NOT be modified
+//  to place orders with any competitors to Kite Tech Ltd, all orders MUST go through the
+//  Kite Tech Ltd platform servers.
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import UIKit
+
+extension UILabel {
+    
+    func setLineHeight(_ lineHeight: CGFloat) {
+        guard let title = self.text else { return }
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.maximumLineHeight = lineHeight
+        paragraphStyle.alignment = self.textAlignment
+        paragraphStyle.lineBreakMode = .byTruncatingTail
+        
+        let attrString = NSMutableAttributedString(string: title)
+        attrString.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraphStyle, range: NSRange(location: 0, length: attrString.length))
+        self.attributedText = attrString
+    }
+    
+    func scaleFont() {
+        if #available(iOS 11.0, *) {
+            self.font = UIFontMetrics.default.scaledFont(for: self.font)
+            self.adjustsFontForContentSizeCategory = true
+        }
+    }
+}
